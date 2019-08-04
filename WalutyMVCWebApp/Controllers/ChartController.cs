6ea7c5f49f65195a10b9c56d30e49df597188c22
Dariusz Jagiellowicz <dariuszjagiellowicz@gmail.com>
2019-08-04 11:00:18 +0200

    Added empty Chart controller and it's views

---
+++ WalutyMVCWebApp/Controllers/ChartController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WalutyMVCWebApp.Controllers
+{
+    public class ChartController : Controller
+    {
+        // GET: Chart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+    }
+}

9b71bbd579ff76db40b0610c73e26b10d191da4a
Dariusz Jagiellowicz <dariuszjagiellowicz@gmail.com>
2019-08-04 11:08:04 +0200

    Added selectable list to Chart controllers and its views

--- WalutyMVCWebApp/Controllers/ChartController.cs
+++ WalutyMVCWebApp/Controllers/ChartController.cs
@@ -1,17 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WalutyBusinessLogic.Services;
 
 namespace WalutyMVCWebApp.Controllers
 {
     public class ChartController : Controller
     {
-        // GET: Chart
-        public ActionResult Index()
+        private readonly ICurrenciesSelectList _currenciesSelectList;
+
+        public ChartController(ICurrenciesSelectList currenciesSelectList)
         {
+            _currenciesSelectList = currenciesSelectList;
+        }
+        // GET: Chart
+        public async Task<ActionResult> Index()
+        {
+            ViewData["currencyCodes"] = await _currenciesSelectList.GetCurrencyCodes(User.Identity.Name);
             return View();
         }
 

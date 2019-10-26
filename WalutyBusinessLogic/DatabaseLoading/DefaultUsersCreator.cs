ec70fd7c513380236c327d75d88c1469c2937bf8
Dariusz Jagiellowicz <dariuszjagiellowicz@gmail.com>
2019-10-26 15:30:07 +0200

    Added creation of few additional default users

---
+++ WalutyBusinessLogic/DatabaseLoading/DefaultUsersCreator.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using WalutyBusinessLogic.Models;
+using WalutyBusinessLogic.Models.Enums;
+
+namespace WalutyBusinessLogic.DatabaseLoading
+{
+    public class DefaultUsersCreator
+    {
+        private static readonly string[] defaultUserNames = new string[] { "kamr@example.com", "zers@example.com", "kali@example.com", "dromero@example.com", "karo@example.com", "zajnb@example.com" };
+        private static readonly string defaultUserPassword = "!1234Qwertyuiop";
+
+        public async static void CreateUsers(UserManager<User> userManager)
+        {
+            foreach (var defaultUser in defaultUserNames)
+            {
+                if (await userManager.FindByNameAsync(defaultUser) == null)
+                {
+                    var user = new User { UserName = defaultUser, Email = defaultUser, SecurityStamp = Guid.NewGuid().ToString(), UserFavoriteCurrencies = new List<UserCurrency>() };
+
+                    var result = await userManager.CreateAsync(user, defaultUserPassword);
+
+                    if (result.Succeeded)
+                    {
+                        await userManager.AddToRoleAsync(user, RolesEnum.User.ToString());
+
+                        Log.Logger.Information($"User {defaultUser} has been created.");
+                    }
+                }
+            }
+        }
+    }
+}

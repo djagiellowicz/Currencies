f6ef98584dce5dc56c955c1fd2987a38ee619549
Dariusz Jagiellowicz <dariuszjagiellowicz@gmail.com>
2019-12-09 21:41:08 +0100

    Added GenericModel with type T Model property

---
+++ WalutyBusinessLogic/Models/GenericModel/GenericModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WalutyBusinessLogic.Models.GenericModel
+{
+    public class GenericModel<T>
+    {
+        public T Model { get; set; }
+    }
+}

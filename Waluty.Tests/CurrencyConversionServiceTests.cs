1d6da7f56c9870a75f51f8d2bf5e29eddd8d5d45
Dariusz Jagiellowicz <dariuszjagiellowicz@gmail.com>
2020-07-13 18:47:52 +0200

    Added creating  of ICurrencyRepositoryMoq in CurrencyConversionServiceTests

--- Waluty.Tests/CurrencyConversionServiceTests.cs
+++ Waluty.Tests/CurrencyConversionServiceTests.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using WalutyBusinessLogic.DatabaseLoading;
+using WalutyBusinessLogic.LoadingFromFile;
 using WalutyBusinessLogic.LoadingFromFile.DatabaseLoading;
 using WalutyBusinessLogic.Services;
 
@@ -12,13 +13,44 @@ namespace Waluty.Tests
     {
         private readonly string _firstCurrencyName = "GBP";
         private readonly string _secondCurrencyName = "EUR";
-        private readonly DateTime _date = DateTime.Now;
+        private readonly DateTime _commonDate = DateTime.Now;
 
-        private ICurrencyConversionService CreateCurrencySonversionService()
+        private ICurrencyRepository CreateICurrencyRepositoryMoq()
         {
+            int firstCurrencyCloseValue = 5;
+            int secondCurrencyCloseValue = 10;
             Mock<ICurrencyRepository> mockRepository = new Mock<ICurrencyRepository>();
+            Currency firstCurrency = CreateCurrency(firstCurrencyCloseValue, _firstCurrencyName, _commonDate);
+            Currency secondCurrency = CreateCurrency(secondCurrencyCloseValue, _secondCurrencyName, _commonDate);
+          
+            mockRepository.Setup(x => x.GetCurrency(_firstCurrencyName)).ReturnsAsync(firstCurrency);
+            mockRepository.Setup(x => x.GetCurrency(_secondCurrencyName)).ReturnsAsync(secondCurrency);
 
-            return null;
+            return mockRepository.Object;
+        }
+
+        private Currency CreateCurrency(int CloseValue, string CurrencyName, DateTime commonDate)
+        {
+            Currency currency = new Currency();
+            List<CurrencyRecord> records = new List<CurrencyRecord>();
+            CurrencyRecord firstRecord = new CurrencyRecord()
+            {
+                Date = commonDate,
+                Close = CloseValue
+            };
+            CurrencyRecord secondRecord = new CurrencyRecord()
+            {
+                Date = new DateTime(1000, 1, 1),
+                Close = 15
+            };
+            
+            records.Add(firstRecord);
+            records.Add(secondRecord);
+
+            currency.Name = _firstCurrencyName;
+            currency.ListOfRecords = records;
+
+            return currency;
         }
 
 

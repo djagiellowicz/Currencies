e9ffed89df6ef72ecb8ff96ca21d131948fc6460
Dariusz Jagiellowicz <dariuszjagiellowicz@gmail.com>
2020-07-13 18:19:27 +0200

    Added CurrencyConversionServiceTests

---
+++ Waluty.Tests/CurrencyConversionServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WalutyBusinessLogic.DatabaseLoading;
+using WalutyBusinessLogic.LoadingFromFile.DatabaseLoading;
+using WalutyBusinessLogic.Services;
+
+namespace Waluty.Tests
+{
+    class CurrencyConversionServiceTests
+    {
+        private readonly string _firstCurrencyName = "GBP";
+        private readonly string _secondCurrencyName = "EUR";
+        private readonly DateTime _date = DateTime.Now;
+
+        private ICurrencyConversionService CreateCurrencySonversionService()
+        {
+            Mock<ICurrencyRepository> mockRepository = new Mock<ICurrencyRepository>();
+
+            return null;
+        }
+
+
+    }
+}

ac289baf042783f18b84bd3824d574d7a19524be
Dariusz Jagiellowicz <dariuszjagiellowicz@gmail.com>
2020-08-11 15:58:41 +0200

    Added references to WalutyBusinessLogic project

--- Waluty.Tests/UserServicesTests.cs
+++ Waluty.Tests/UserServicesTests.cs
@@ -1,10 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using System;
+using WalutyBusinessLogic.DatabaseLoading;
+using WalutyBusinessLogic.Models;
 
 namespace Waluty.Tests
 {
     public class UserServicesTests
     {
+        private void CreateUserManagerMoq()
+        {
+            Mock<UserManager<User>> userManagerMock = new Mock<UserManager<User>>(
+                new Mock<IUserStore<User>>().Object,
+                new Mock<IOptions<IdentityOptions>>().Object,
+                new Mock<IPasswordHasher<User>>().Object,
+                new IUserValidator<User>[0],
+                new IPasswordValidator<User>[0],
+                new Mock<ILookupNormalizer>().Object,
+                new Mock<IdentityErrorDescriber>().Object,
+                new Mock<IServiceProvider>().Object,
+                new Mock<ILogger<UserManager<User>>>().Object);
+
+
+            DbContextOptions contextOptions = new DbContextOptions<WalutyDBContext>();
+              
+
+        }
+
     }
 }

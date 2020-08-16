650c67c152760c3973226cae577cd98c71b941ad
Dariusz Jagiellowicz <dariuszjagiellowicz@gmail.com>
2020-08-16 21:22:07 +0200

    Added CreateUserServices in tests

--- Waluty.Tests/UserServicesTests.cs
+++ Waluty.Tests/UserServicesTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
@@ -6,12 +7,13 @@ using Moq;
 using System;
 using WalutyBusinessLogic.DatabaseLoading;
 using WalutyBusinessLogic.Models;
+using WalutyBusinessLogic.Services;
 
 namespace Waluty.Tests
 {
     public class UserServicesTests
     {
-        private void CreateUserManagerMoq()
+        private void CreateUserServices()
         {
             Mock<UserManager<User>> userManagerMock = new Mock<UserManager<User>>(
                 new Mock<IUserStore<User>>().Object,
@@ -24,9 +26,28 @@ namespace Waluty.Tests
                 new Mock<IServiceProvider>().Object,
                 new Mock<ILogger<UserManager<User>>>().Object);
 
+            Mock<IMapper> mapperMock = new Mock<IMapper>();
+            Mock<IMapper> iMapperMock = new Mock<IMapper>();
+            Mock<IPasswordValidator<User>> iPasswordValidatorMock = new Mock<IPasswordValidator<User>>();
+            Mock<RoleManager<IdentityRole>> roleManagerMock = new Mock<RoleManager<IdentityRole>>();
+
 
             DbContextOptions contextOptions = new DbContextOptions<WalutyDBContext>();
-              
+            var options = new DbContextOptionsBuilder<WalutyDBContext>().UseInMemoryDatabase("test").Options;
+
+            WalutyDBContext context = new WalutyDBContext(options);
+
+            //Give proper user
+            userManagerMock.Setup(x => x.FindByIdAsync("1")).ReturnsAsync(new User());
+
+            UserServices userServices = new UserServices(userManagerMock.Object,
+                context,mapperMock.Object,
+                iPasswordValidatorMock.Object,
+                roleManagerMock.Object
+                );
+          
+
+
 
         }
 

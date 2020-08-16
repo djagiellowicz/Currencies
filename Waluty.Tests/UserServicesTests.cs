cdffaf68f60d169b2c90884bbcff61cc6d48d0ec
Dariusz Jagiellowicz <dariuszjagiellowicz@gmail.com>
2020-08-16 21:51:09 +0200

    Using production AutoMapper in tests, not mocked version

--- Waluty.Tests/UserServicesTests.cs
+++ Waluty.Tests/UserServicesTests.cs
@@ -5,9 +5,12 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Moq;
 using System;
+using WalutyBusinessLogic.AutoMapper.Profiles;
 using WalutyBusinessLogic.DatabaseLoading;
 using WalutyBusinessLogic.Models;
+using WalutyBusinessLogic.Models.DTO;
 using WalutyBusinessLogic.Services;
+using Xunit;
 
 namespace Waluty.Tests
 {
@@ -26,28 +29,33 @@ namespace Waluty.Tests
                 new Mock<IServiceProvider>().Object,
                 new Mock<ILogger<UserManager<User>>>().Object);
 
-            Mock<IMapper> mapperMock = new Mock<IMapper>();
-            Mock<IMapper> iMapperMock = new Mock<IMapper>();
+            IMapper iMapper = new MapperConfiguration(c => c.AddProfile<UserProfileMap>()).CreateMapper();
+
             Mock<IPasswordValidator<User>> iPasswordValidatorMock = new Mock<IPasswordValidator<User>>();
             Mock<RoleManager<IdentityRole>> roleManagerMock = new Mock<RoleManager<IdentityRole>>();
-
 
             DbContextOptions contextOptions = new DbContextOptions<WalutyDBContext>();
             var options = new DbContextOptionsBuilder<WalutyDBContext>().UseInMemoryDatabase("test").Options;
 
             WalutyDBContext context = new WalutyDBContext(options);
 
+            User userToReturn = new User() { Id = "1234", Email = "John@john.com" };
+
+
             //Give proper user
-            userManagerMock.Setup(x => x.FindByIdAsync("1")).ReturnsAsync(new User());
+            userManagerMock.Setup(x => x.FindByIdAsync("1234")).ReturnsAsync(userToReturn);
 
             UserServices userServices = new UserServices(userManagerMock.Object,
-                context,mapperMock.Object,
+                context,
+                iMapper,
                 iPasswordValidatorMock.Object,
-                roleManagerMock.Object
-                );
-          
+                roleManagerMock.Object);
 
+        }
 
+        [Fact]
+        public void CreateUserServices_Correct_User_Is_Returned()
+        {
 
         }
 

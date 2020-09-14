926f670443b8436d65cf65f02a57886f4cd14e42
Dariusz Jagiellowicz <dariuszjagiellowicz@gmail.com>
2020-09-14 15:47:55 +0200

    Added method GetLoggedUserFavCurrencies

--- WalutyBusinessLogic/Services/FavoritesService.cs
+++ WalutyBusinessLogic/Services/FavoritesService.cs
@@ -1,10 +1,35 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using WalutyBusinessLogic.DatabaseLoading;
+using WalutyBusinessLogic.LoadingFromFile;
+using WalutyBusinessLogic.Models;
 
 namespace WalutyBusinessLogic.Services
 {
-    public class FavoritesService
+    public class FavoritesService : IFavoritesService
     {
+        private readonly UserManager<User> _userManager;
+        private readonly WalutyDBContext _context;
+
+        public FavoritesService(UserManager<User> userManager, WalutyDBContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+        public async Task<List<Currency>> GetLoggedUserFavCurrenciesAsync(ClaimsPrincipal user)
+        {
+            var loggedInUser = await _userManager.Users
+                    .Include(u => u.UserFavoriteCurrencies)
+                    .SingleAsync(u => u.UserName == user.Identity.Name);
+
+            List<Currency> currencies = _context.UsersCurrencies.Where(u => u.User.Id == loggedInUser.Id).Select(x => x.Currency).ToList();
+
+            return currencies;
+        }
     }
 }

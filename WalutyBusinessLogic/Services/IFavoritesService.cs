c3c2a3fd1e4afa4619aa39c4fbcb8350095c7c3d
Dariusz Jagiellowicz <dariuszjagiellowicz@gmail.com>
2020-09-15 15:30:44 +0200

    Filled methods for IFavoritesService interface

--- WalutyBusinessLogic/Services/IFavoritesService.cs
+++ WalutyBusinessLogic/Services/IFavoritesService.cs
@@ -1,6 +1,14 @@
-﻿namespace WalutyBusinessLogic.Services
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using WalutyBusinessLogic.LoadingFromFile;
+
+namespace WalutyBusinessLogic.Services
 {
     public interface IFavoritesService
     {
+        Task<List<Currency>> GetLoggedUserFavCurrencies(ClaimsPrincipal user);
+        Task<bool> AddFavCurrency(int currencyId, ClaimsPrincipal user);
+        Task<bool> DeleteFavCurrency(int currencyId, ClaimsPrincipal user);
     }
 }

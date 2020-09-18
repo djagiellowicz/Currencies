0cfd35e4c1234e87cff975a3a38c4b1738cb2a5b
Dariusz Jagiellowicz <dariuszjagiellowicz@gmail.com>
2020-09-18 21:14:43 +0200

    Added empty IUserCurrencyRepository and UserCurrencyRepository

---
+++ WalutyBusinessLogic/DatabaseLoading/UserCurrencyRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WalutyBusinessLogic.DatabaseLoading
+{
+    public class UserCurrencyRepository : IUserCurrencyRepository
+    {
+    }
+}

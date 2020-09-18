5eecc9614295fa9146f8b0608bc418842952cab4
Dariusz Jagiellowicz <dariuszjagiellowicz@gmail.com>
2020-09-18 21:41:33 +0200

    Filled IUserCurrencyRepository and UserCurrencyRepository

--- WalutyBusinessLogic/DatabaseLoading/IUserCurrencyRepository.cs
+++ WalutyBusinessLogic/DatabaseLoading/IUserCurrencyRepository.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using WalutyBusinessLogic.LoadingFromFile;
+using WalutyBusinessLogic.Models;
 
 namespace WalutyBusinessLogic.DatabaseLoading
 {
-    class IUserCurrencyRepository
+    public interface IUserCurrencyRepository
     {
+        Task<List<Currency>> GetUserFavoriteCurrencies(string userId);
+
+        Task<UserCurrency> GetUserFavoriteCurrency(string userId, int currencyId);
+
+        void AddUserFavoriteCurrency(UserCurrency userCurrency);
+
+        void DeleteUserFavoriteCurrency(UserCurrency userCurrency);
+
     }
 }
--- WalutyBusinessLogic/DatabaseLoading/UserCurrencyRepository.cs
+++ WalutyBusinessLogic/DatabaseLoading/UserCurrencyRepository.cs
@@ -1,10 +1,46 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using WalutyBusinessLogic.LoadingFromFile;
+using WalutyBusinessLogic.Models;
 
 namespace WalutyBusinessLogic.DatabaseLoading
 {
     public class UserCurrencyRepository : IUserCurrencyRepository
     {
+        private readonly WalutyDBContext _context;
+
+        public UserCurrencyRepository(WalutyDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Currency>> GetUserFavoriteCurrencies(string userId)
+        {
+            List<Currency> favCurrencies = await _context.UsersCurrencies.Where(u => u.User.Id == userId).Select(x => x.Currency).ToListAsync();
+
+            return favCurrencies;
+        }
+
+        public async Task<UserCurrency> GetUserFavoriteCurrency(string userId, int currencyId)
+        {
+            UserCurrency userCurrency = await _context.UsersCurrencies.SingleAsync(x => x.User.Id == userId && x.CurrencyId == currencyId);
+
+            return userCurrency;
+        }
+
+        public void AddUserFavoriteCurrency(UserCurrency userCurrency)
+        {
+            _context.UsersCurrencies.Add(userCurrency);
+            _context.SaveChanges();
+        }
+
+        public void DeleteUserFavoriteCurrency(UserCurrency userCurrency)
+        {
+            _context.UsersCurrencies.Remove(userCurrency);
+            _context.SaveChanges();
+        }
+
     }
 }

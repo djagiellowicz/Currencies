37022ba34b8c61172746fa9c9c3b7f19519ef430
Dariusz Jagiellowicz <dariuszjagiellowicz@gmail.com>
2020-11-09 12:45:22 +0100

    Added GetFlag method to ConfigExt

--- WalutyBusinessLogic/Extensions/ConfigExt.cs
+++ WalutyBusinessLogic/Extensions/ConfigExt.cs
@@ -1,10 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 
 namespace WalutyBusinessLogic.Extensions
 {
-    class ConfigExt
+    public static class ConfigExt
     {
+        private static string trueValue = "true";
+        private static string falseValue = "false";
+
+        public static bool GetFlag(this IConfiguration configuration, string key)
+        {
+            string value = configuration.GetSection("Flags")[key] ?? throw new ArgumentNullException();
+
+            if (trueValue.Equals(value.ToLower())) return true;
+            if (falseValue.Equals(value.ToLower())) return true;
+
+            throw new ArgumentOutOfRangeException("Wrong value at: " + key);
+        }
     }
 }
